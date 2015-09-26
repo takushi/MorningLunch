79e26d945a3517efe8fba8c89053d24bc274c872
takushi <takushi@mfmf.me>
2015-09-26 10:53:56 +0900

    Initial Commit

---
+++ MorningLunch/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MorningLunch
+//
+//  Created by Homma Takushi on 2015/09/26.
+//  Copyright © 2015年 mfmf.me. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
